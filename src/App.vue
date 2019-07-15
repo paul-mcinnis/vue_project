87479da5ec23b20200bfa0b4c50fc525c358763e
Paul McInnis <mcinnis.paul@gmail.com>
2019-07-14 22:44:23 -0500

    configured gninx proxy

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      <router-link to="/404">404</router-link>
     </div>
     <router-view />
   </div>

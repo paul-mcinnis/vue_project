2ed8175db22b9bbc136c2a78535cdb6576e971fa
Paul McInnis <mcinnis.paul@gmail.com>
2019-07-20 11:53:58 -0500

    updated 404 error page

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="ocean-bg" :style="oceanBg">
-    <h1>404 Page not found !!</h1>
+    <h1>Sorry</h1>
+    <h2>We can't find that page</h2>
+    <router-link to="/">
+      Would you like to
+      <strong>Go to Home?</strong>
+    </router-link>
   </div>
 </template>
 
@@ -16,3 +21,16 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+h1 {
+  color: red;
+}
+h2 {
+  color: blue;
+}
+strong {
+  font-style: italic;
+  text-decoration: underline;
+}
+</style>

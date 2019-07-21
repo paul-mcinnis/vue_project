1a25deff9aaf4624b7d03f5521c52343388a4f79
Paul McInnis <mcinnis.paul@gmail.com>
2019-07-21 14:19:32 -0500

    added transparent background on scrolling

--- src/components/TheNavBar.vue
+++ src/components/TheNavBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="nav">
+  <div id="nav" class="navbar" :class="{ 'navbar--hidden': hideBackground }">
     <router-link to="/">Home</router-link>|
     <router-link to="/about">About</router-link>
   </div>
@@ -9,11 +9,34 @@
 import { Component, Vue } from 'vue-property-decorator'
 
 @Component
-export default class TheNavBar extends Vue {}
+export default class TheNavBar extends Vue {
+  data() {
+    return {
+      hideBackground: true
+    }
+  }
+  mounted() {
+    window.addEventListener('scroll', this.scrolled)
+  }
+  beforeDestroy() {
+    window.removeEventListener('scroll', this.scrolled)
+  }
+
+  scrolled() {
+    // Get the current scroll position
+    const currentScrollPosition =
+      window.pageYOffset || document.documentElement.scrollTop
+    if (currentScrollPosition <= 150) {
+      this.hideBackground = true
+      return
+    }
+    this.hideBackground = false
+  }
+}
 </script>
 
 <style scoped>
-#nav {
+.navbar {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -21,24 +44,28 @@ export default class TheNavBar extends Vue {}
   color: #2c3e50;
   padding: 20px;
   position: fixed;
+  background-color: rgba(255, 255, 255, 0.8);
   top: 0;
   width: 100%;
-  background-color: white;
-  opacity: 0.8;
   z-index: 100;
 }
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+
+.navbar--hidden {
+  background-color: transparent;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+.navbar a {
+  font-weight: bold;
+  color: black;
+}
+
+.navbar a.router-link-exact-active {
+  color: #669999;
 }
 
 /* mobile */
 @media (max-width: 544px) {
-  #nav {
+  .navbar {
     padding: 10px;
   }
 }
